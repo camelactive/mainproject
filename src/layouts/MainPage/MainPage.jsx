9983741a90143b8bc68f5e59bc96b7642ef45808
Igor <camelactive@inbox.ru>
2023-06-11 15:35:27 +0500

    change height to min and full

--- src/layouts/MainPage/MainPage.jsx
+++ src/layouts/MainPage/MainPage.jsx
@@ -3,7 +3,7 @@ import TopMenu from "../../components/TopMenu/TopMenu"
 
 const MainPage = () => {
   return (
-    <div className="h-screen bg-slate-500 text-cyan-600">
+    <div className="min-h-screen h-full bg-slate-500 text-cyan-600">
         <TopMenu/>
         <div className="flex justify-center w-4/5 bg-white mx-auto mt-3 border-l-4 border-r-4 border-orange-700">
             <Outlet/>
